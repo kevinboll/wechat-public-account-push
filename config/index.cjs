198031915ab5ed978e3bd4318d3167c9f99f6aa8
kevinboll <132773516+kevinboll@users.noreply.github.com>
2023-05-20 00:09:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7XM05_o8zJG6DLaidTlp6-RhqGg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'mlTmQAJdLHUuMpUprJKAKNm7M1wg-hTIRoe_MZmOM3w',
+      useTemplateId: 'tjtNbM7WlgnC1ajkl0PzNdrifRBKhSHrCd2dM5CwGeY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '7-27',
       festivals: [
@@ -71,11 +71,11 @@ const USER_CONFIG = {
   ],
     },{
       // 想要发送的人的名字
-      name: '王宝贝',
+      name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7XM05weWYftE9b_B7j8_DLTnfTQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'blMieWZ4QJtK4y_xuhl7VLZZ51XW5FddPgkXZJC35mA',
+      useTemplateId: 'tjtNbM7WlgnC1ajkl0PzNdrifRBKhSHrCd2dM5CwGeY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '7-27',
       festivals: [
